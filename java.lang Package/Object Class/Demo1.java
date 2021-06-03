efa9f8299d4bd0a0674557d4c5c2aca4a7951623
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-06-03 17:24:02 +0530

    Update and rename New Text Document.txt to Demo1.java

--- java.lang Package/Object Class/Demo1.java
+++ java.lang Package/Object Class/Demo1.java
@@ -2,7 +2,7 @@ import java.lang.*;
 import java.util.Scanner;
 
 
-public class Main {
+public class Demo1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("In Main:");
