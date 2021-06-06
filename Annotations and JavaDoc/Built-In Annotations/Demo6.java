9e6eb790778b5f0dfc1f07908c561e81b1cd3409
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-06-06 13:57:04 +0530

    Create Demo6.java

---
+++ Annotations and JavaDoc/Built-In Annotations/Demo6.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.Scanner;
+
+// Functional Interface means the interface is having the only one method.
+@FunctionalInterface
+interface My {
+    public void Show();
+}
+
+public class Demo6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("In Main:");
+
+    }
+}
