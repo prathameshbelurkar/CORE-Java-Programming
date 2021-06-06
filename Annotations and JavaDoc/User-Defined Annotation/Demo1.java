81d36ed213a8f3de6c8ac7cd66f083e370767eb0
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-06-06 16:17:32 +0530

    Update Demo1.java

--- Annotations and JavaDoc/User-Defined Annotation/Demo1.java
+++ Annotations and JavaDoc/User-Defined Annotation/Demo1.java
@@ -1,1 +1,24 @@
-Demo1
+import java.lang.*;
+import java.util.Scanner;
+
+// User-Defined Annotation
+@interface MyAnno {
+    String Name();
+
+    String Project();
+
+    String Date();
+
+    String Version() default "13";
+}
+
+// Annotation at Class
+@MyAnno(Name = "Prathamesh", Project = "Bank", Date = "1/1/2020")
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("In Main:");
+        int x;
+
+    }
+}
