9cc6894c2164749c6975625a8b40c3bde3d52db9
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-06-07 15:40:47 +0530

    Add files via upload

---
+++ Java IO Streams/Demo2/Demo2.java
@@ -0,0 +1,33 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.*;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("In Main:");
+
+        try {
+            FileOutputStream fos = new FileOutputStream("C:/Users/SANJAY/Downloads/Demo2/Test.txt");
+            String str = "Learn Java Programming";
+
+            byte b[] = str.getBytes();
+            for (byte x:b) {
+                fos.write(x);
+            }
+             
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+
+
+
+    }
+}
