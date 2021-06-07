27c81a9008b6ecee5971f7ff0ec1b3d95947766a
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-06-07 15:41:19 +0530

    Update Demo2.java

--- Java IO Streams/Demo2/Demo2.java
+++ Java IO Streams/Demo2/Demo2.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.util.Scanner;
 
 
-public class Main {
+public class Demo2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("In Main:");
