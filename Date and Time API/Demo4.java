5e45791d7d2e6096f73c942062c7dc5c7891d575
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-06-12 15:27:42 +0530

    Create Demo4.java

---
+++ Date and Time API/Demo4.java
@@ -0,0 +1,35 @@
+import java.lang.*;
+import java.time.*;
+import java.util.Scanner;
+
+
+public class Demo4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("In Main:");
+
+        // ZONEDDATETIME
+        // ZonedDateTime zdt = ZonedDateTime.now();
+        // System.out.println(zdt);
+
+        // ZonedDateTime zdt = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        // System.out.println(zdt);
+
+        // OFFSETDATETIME
+        // OffsetDateTime odt = OffsetDateTime.now();
+        // System.out.println(odt);
+
+        // MONTHDAY
+        // MonthDay md = MonthDay.now();
+
+        // PERIOD
+        // Period p = Period.of(2,2,10);
+        // System.out.println(p);
+        // System.out.println(p.addTo(LocalDate.now()));
+
+        // INSTANT
+        // Instant i = Instant.now();
+
+
+    }
+}
