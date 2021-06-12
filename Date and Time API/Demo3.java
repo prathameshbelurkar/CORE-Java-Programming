f7d033ee662732dfe1fe965919ee97022a817b30
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-06-12 12:18:12 +0530

    Create Demo3.java

---
+++ Date and Time API/Demo3.java
@@ -0,0 +1,49 @@
+import java.lang.*;
+import java.time.*;
+import java.util.Date;
+import java.util.Scanner;
+
+
+public class Demo3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("In Main:");
+
+        // LOCAL DATE
+
+        // LocalDate d = LocalDate.now();
+        // System.out.println(d);
+
+        // LocalDate d = LocalDate.now(Clock.systemDefaultZone());
+        // System.out.println(d);
+
+        // LocalDate d = LocalDate.now(ZoneId.of("Asia/Kolkata"));
+        // System.out.println(d);
+
+        // LocalDate d = LocalDate.of(2021, Month.JUNE, 10);
+        // System.out.println(d);
+
+        // LocalDate d = LocalDate.ofEpochDay(1000);
+        // System.out.println(d);
+
+        // LocalDate d = LocalDate.parse("2020-01-03");
+        // LocalDate d1 = d.plusMonths(6);
+        // System.out.println(d);
+        // System.out.println(d1);
+
+        // LOCALTIME
+
+        // LocalTime t = LocalTime.now();
+        // System.out.println(t);
+        // LocalTime t1 = t.minusHours(3);
+        // System.out.println(t1);
+
+        // LOCALDATETIME
+
+        // LocalDateTime dt = LocalDateTime.now();
+        // System.out.println(dt);
+        
+
+
+    }
+}
