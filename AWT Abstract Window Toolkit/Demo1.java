e6f073db64b8b8c482c2f79945328b003a127ebf
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-06-18 14:01:44 +0530

    Add files via upload

---
+++ AWT Abstract Window Toolkit/Demo1.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.awt.*;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Frame f = new Frame("My First App");
+        f.setLayout(new FlowLayout());
+
+        Button b = new Button("Ok");
+        Label l = new Label("Name ");
+        TextField tf = new TextField(20);
+
+        f.add(l);
+        f.add(tf);
+        f.add(b);
+
+
+        f.setSize(300, 300);
+        f.setVisible(true);
+
+    }
+}
