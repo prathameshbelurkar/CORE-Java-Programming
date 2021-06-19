13708f0e53c594a38cd04694e9cbac0addf97c23
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-06-19 10:42:42 +0530

    Create Demo4.java

---
+++ AWT Abstract Window Toolkit/Demo4.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class MyFrame extends Frame implements ActionListener {
+    int count=0;
+    Label l;
+    Button b;
+
+    public MyFrame() {
+        super("Button Demo");
+        l = new Label("   "+count);
+        b = new Button("Click");
+        b.addActionListener(this);
+
+        setLayout(new FlowLayout());
+        add(l);
+        add(b);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        l.setText("   "+count);
+    }
+}
+
+
+public class Demo4 {
+    public static void main(String[] args) {
+        MyFrame f = new MyFrame();
+        f.setSize(400, 400);
+        f.setVisible(true);
+    }
+}
